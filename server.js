6e0adc5df1346e44402844203589d5c035ed7086
ravi2809 <guptaravi2809@gmail.com>
2017-08-14 20:47:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function(req, res) {
-    res.send('Hi, this is article one. Welcome here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 })
 
 app.get('/article-two', function(req, res) {
-    res.send('Hi, this is article two. Welcome here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 })
 
 app.get('/article-three', function(req, res) {
-    res.send('Hi, this is article three. Welcome here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 })
 
 app.get('/ui/main.js', function( req, res) {
