9ca54fdd9102d3d3c84791b4ceb9bdf17020760c
ravi2809 <guptaravi2809@gmail.com>
2017-08-15 02:05:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,21 @@
 var button = document.getElementById('counter');
-counter =0;
 button.onclick = function(){
     
-    counter = counter +1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //create a request
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if( request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    }
+    
+    //Make the request
+    request.open('GET','http://guptaravi2809.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
