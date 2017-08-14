2f7dadb434c825ca0e0a5cacb22c9770d53e951d
ravi2809 <guptaravi2809@gmail.com>
2017-08-15 01:48:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,7 @@
-console.log('Loaded!');
-
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value boss';
-
-var img = document.getElementById('img');
-var marginLeft =0;
-var flag =0;
-function moveRight()
-{
-marginLeft = marginLeft+ 10;
-img.style.marginLeft = marginLeft + 'px';
-}
-
-function moveLeft()
-{
-marginLeft = marginLeft- 10;
-img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    if (img.style.marginLeft <= '1000px')
-    {
-    var interval = setInterval(moveRight,50);
-    }
-    else 
-    var interval2 = setInterval(moveLeft,50);
+var button = document.getElementById('counter');
+button.onclick = function(){
     
+    counter = counter +1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
