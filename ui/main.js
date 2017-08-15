3aa87cba36d5ac37c9eeacd221a9c6f810803f58
ravi2809 <guptaravi2809@gmail.com>
2017-08-15 10:59:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,16 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-     var names = ['name1','name2','name3','name4'];
+    
+     var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if( request.status === 200) {
+                
+                var names = request.responseText;
+                names = JSON.parse(names);
      var list = '';
      for(var i =0; i<names.length; i++)
      {
@@ -35,7 +44,15 @@ submit.onclick = function() {
     
     var ul = document.getElementById("namelist");
     ul.innerHTML = list;
+               
+            }
+        }
+    }
+    
+    //Make the request
+    request.open('GET','http://guptaravi2809.imad.hasura-app.io/submit-name?name='+name, true);
+    request.send(null);
+};
+     
     
     
-    
-}
