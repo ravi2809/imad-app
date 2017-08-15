cf47304ad2c1075aaccdb78b083896dbb1b8f82e
ravi2809 <guptaravi2809@gmail.com>
2017-08-15 11:02:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,10 +22,12 @@ button.onclick = function(){
 
 //submit name
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
+    
+    var nameInput = document.getElementById('name');
+var name = nameInput.value;
     
      var request = new XMLHttpRequest();
     
